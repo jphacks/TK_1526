0444f8b156dbf331454b62cb242076347b435884
shunsakai <1272848773@mail.ecc.u-tokyo.ac.jp>
2015-11-28 19:56:01 +0900

    Add extractTimeFromSentence func

---
+++ extract_time_from_sentence_func.swift
@@ -0,0 +1,24 @@
+var sentence:String = "Is it able to extract integer from long sentence, like 'Please wait for three minutes yeah', and the result is five minutes walking"
+
+var numDictionary = [
+    "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+    "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16,
+    "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20
+]
+
+func extractTimeFromSentence(inout str:String) -> (Int) {
+    var numInt: Int?
+    let splitedSentenceArray: [String] = str.componentsSeparatedByString(" ")
+    for tuple in splitedSentenceArray.enumerate() {
+        if ((tuple.element == "minute") || (tuple.element == "minutes")) {
+            print("\(tuple.index)")
+            let indexNum = tuple.index-1
+            let numStr = splitedSentenceArray[indexNum]
+            numInt = numDictionary[numStr]
+        }
+    }
+    return numInt!
+}
+
+let num = extractTimeFromSentence(&sentence)
+print(num)
